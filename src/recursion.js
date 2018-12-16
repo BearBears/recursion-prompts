4da6dea3d508599df5dd64b8887c2f8df3d1875a
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2018-12-16 11:08:57 -0800

    "Added exponent"

--- src/recursion.js
+++ src/recursion.js
@@ -98,14 +98,17 @@ var range = function(x, y) {
 	}
 };
 
-console.log(range(4,1));
-
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+	if (exp === 0) return 1;
+	else if (exp === 1) return base;
+	else if (exp === -1) return 1/base;
+	else if (exp > 1) return base*(exponent(base,exp-1));
+	else if (exp < 1) return 1/exponent(base, -exp);
 };
 
 // 8. Determine if a number is a power of two.
